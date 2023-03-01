de56ba86e9e8ae56b2e61e1ae1038466a06bc62c
Le Duy Anh <lenduya@gmail.com>
2023-03-01 17:51:48 +0100

    refactor (model): set default values for Crime

--- app/src/main/java/com/example/criminalintentcompose/data/model/Crime.kt
+++ app/src/main/java/com/example/criminalintentcompose/data/model/Crime.kt
@@ -4,8 +4,11 @@ import java.util.Date
 import java.util.UUID
 
 data class Crime(
-  val id: UUID,
-  val title: String,
-  val date: Date,
-  val isSolved: Boolean
+  val id: UUID = UUID.fromString(NEW_CRIME_ID),
+  val title: String = "",
+  val date: Date = Date(),
+  val isSolved: Boolean = false
 )
+
+
+const val NEW_CRIME_ID = "new crime"
